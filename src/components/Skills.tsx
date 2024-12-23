32ec78cd5f6ecdb943bf76ec938192bdb8213885
efe <efe13dev@gmail.com>
2024-12-23 10:03:53 +0100

    change skills colors

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,64 +1,78 @@
-"use client";
+'use client';
 
 import { Code, Server, Database, Palette, Globe } from 'lucide-react';
-import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+import { InfiniteMovingCards } from './ui/infinite-moving-cards';
 
 const skills = [
   {
-    quote: "",
-    name: <div className="flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl">
-      <Code className='text-[#638ec6] w-10 h-10 shrink-0' />
-      <span className="text-[#638ec6] whitespace-nowrap">HTML</span>
-    </div>,
-    title: "Frontend"
+    quote: '',
+    name: (
+      <div className='flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl'>
+        <Code className='text-[#b87163] w-10 h-10 shrink-0' />
+        <span className='text-[#b87163] whitespace-nowrap'>HTML</span>
+      </div>
+    ),
+    title: 'Frontend'
   },
   {
-    quote: "",
-    name: <div className="flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl">
-      <Code className='text-[#a2b7d1] w-10 h-10 shrink-0' />
-      <span className="text-[#a2b7d1] whitespace-nowrap">React</span>
-    </div>,
-    title: "Frontend"
+    quote: '',
+    name: (
+      <div className='flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl'>
+        <Code className='text-[#4FB3D9] w-10 h-10 shrink-0' />
+        <span className='text-[#4FB3D9] whitespace-nowrap'>React</span>
+      </div>
+    ),
+    title: 'Frontend'
   },
   {
-    quote: "",
-    name: <div className="flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl">
-      <Globe className='text-[#eceff3] w-10 h-10 shrink-0' />
-      <span className="text-[#eceff3] whitespace-nowrap">Next.js</span>
-    </div>,
-    title: "Frontend"
+    quote: '',
+    name: (
+      <div className='flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl'>
+        <Globe className='text-[#7ab39f] w-10 h-10 shrink-0' />
+        <span className='text-[#7ab39f] whitespace-nowrap'>Next.js</span>
+      </div>
+    ),
+    title: 'Frontend'
   },
   {
-    quote: "",
-    name: <div className="flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl">
-      <Server className='text-[#a2b7d1] w-10 h-10 shrink-0' />
-      <span className="text-[#a2b7d1] whitespace-nowrap">Node.js</span>
-    </div>,
-    title: "Backend"
+    quote: '',
+    name: (
+      <div className='flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl'>
+        <Server className='text-[#8ba88a] w-10 h-10 shrink-0' />
+        <span className='text-[#8ba88a] whitespace-nowrap'>Node.js</span>
+      </div>
+    ),
+    title: 'Backend'
   },
   {
-    quote: "",
-    name: <div className="flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl">
-      <Code className='text-[#324f75] w-10 h-10 shrink-0' />
-      <span className="text-[#324f75] whitespace-nowrap">TypeScript</span>
-    </div>,
-    title: "Language"
+    quote: '',
+    name: (
+      <div className='flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl'>
+        <Code className='text-[#4A7BB7] w-10 h-10 shrink-0' />
+        <span className='text-[#4A7BB7] whitespace-nowrap'>TypeScript</span>
+      </div>
+    ),
+    title: 'Language'
   },
   {
-    quote: "",
-    name: <div className="flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl">
-      <Palette className='text-[#638ec6] w-10 h-10 shrink-0' />
-      <span className="text-[#638ec6] whitespace-nowrap">Tailwind CSS</span>
-    </div>,
-    title: "Styling"
+    quote: '',
+    name: (
+      <div className='flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl'>
+        <Palette className='text-[#2FB4D9] w-10 h-10 shrink-0' />
+        <span className='text-[#2FB4D9] whitespace-nowrap'>Tailwind CSS</span>
+      </div>
+    ),
+    title: 'Styling'
   },
   {
-    quote: "",
-    name: <div className="flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl">
-      <Database className='text-[#324f75] w-10 h-10 shrink-0' />
-      <span className="text-[#324f75] whitespace-nowrap">MySQL</span>
-    </div>,
-    title: "Database"
+    quote: '',
+    name: (
+      <div className='flex items-center justify-center min-w-[250px] w-[250px] gap-3 text-2xl'>
+        <Database className='text-[#2B5C8E] w-10 h-10 shrink-0' />
+        <span className='text-[#2B5C8E] whitespace-nowrap'>MySQL</span>
+      </div>
+    ),
+    title: 'Database'
   }
 ];
 
@@ -74,10 +88,10 @@ export function Skills() {
       <div className='h-32 rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden'>
         <InfiniteMovingCards
           items={skills}
-          direction="right"
-          speed="normal"
+          direction='right'
+          speed='normal'
           pauseOnHover={true}
-          className="py-4"
+          className='py-4'
         />
       </div>
     </section>
