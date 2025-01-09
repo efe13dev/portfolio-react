474367a64b56ce2b1db3af17f4fbc45930abb908
efe <efe13dev@gmail.com>
2025-01-09 20:04:22 +0100

    change font on hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -46,7 +46,8 @@ export function Hero() {
       {/* Columna de texto */}
       <div className='space-y-6'>
         <motion.h1
-          className='text-5xl font-bold font-heading bg-clip-text text-transparent bg-gradient-to-r from-[#a2b7d1] via-[#324f75] to-[#638ec6]'
+          className='text-5xl font-bold font-heading bg-clip-text text-transparent bg-gradient-to-r from-[#a2b7d1] via-[#324f75] to-[#638ec6] [text-wrap:balance]'
+          style={{ WebkitBackgroundClip: 'text' }}
           variants={fadeInUp}
           initial='initial'
           whileInView='whileInView'
